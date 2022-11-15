a5817935666a35ccfc7f821890ca0a88508ce887
eivv77 <dlya.kf.2002@mail.ru>
2022-11-15 22:20:59 +0400

    Добавьте файлы проекта.

---
+++ WFA_xmlread/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace WFA_xmlread
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 29);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 13;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(10, 54);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(243, 121);
+            this.listBox1.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 192);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Show";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(103, 193);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(2);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(151, 20);
+            this.dateTimePicker1.TabIndex = 16;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 7);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Seçilən Tarix";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(11, 267);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(74, 28);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "Export Excel";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(318, 29);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(742, 287);
+            this.dataGridView1.TabIndex = 19;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(318, 322);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1159, 543);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WFA_xmlread/Form1.cs
@@ -0,0 +1,130 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace WFA_xmlread
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string theDate;
+
+        public void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            theDate = dateTimePicker1.Value.ToString("dd.MM.yyyy");
+            listBox1.Items.Clear();
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = theDate;
+            /*ServicePointManager.SecurityProtocol =
+                (SecurityProtocolType)3072 | // TLS 1.2
+                (SecurityProtocolType)768 | // TLS 1.1
+                (SecurityProtocolType)192;   // TLS 1.0
+            ServicePointManager.SecurityProtocol =
+                SecurityProtocolType.Ssl3 |
+                SecurityProtocolType.Tls |
+                SecurityProtocolType.Tls11 |
+                SecurityProtocolType.Tls12 |
+                SecurityProtocolType.Tls13;
+            ServicePointManager.ServerCertificateValidationCallback = (snder, cert, chain, error) => true;
+            ServicePointManager.Expect100Continue = true;
+            string url = $"https://www.cbar.az/currencies/{theDate}.xml";
+            // use the XmlTextReader to get the xml at the ul
+            XmlTextReader reader = new XmlTextReader(url);
+            string sp = "";
+            int k = 0;
+            while (reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Text)
+                {
+
+                    k++;
+                    sp += reader.Value + " ";
+                    if (k % 3 == 0)
+                    {
+                        listBox1.Items.Add(sp);
+
+                        sp = "";
+                    }
+                }
+            }*/
+            XmlDocument doc = new XmlDocument();
+            doc.Load($"https://www.cbar.az/currencies/{theDate}.xml");
+            XmlElement root = doc.DocumentElement;
+            XmlNodeList nodes = root.SelectNodes("/ValCurs/ValType/Valute/Value");
+            var usdNode = nodes[4];
+            var euroNode= nodes[5];
+            var rublNode = nodes[38];
+            listBox1.Items.Add(" USD " + usdNode.InnerText);
+            listBox1.Items.Add(" EUR " + euroNode.InnerText);
+            listBox1.Items.Add(" RUB " + rublNode.InnerText);
+
+        }
+
+        
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string ExcelFileLocation = ($@"D:\codding\Currency_{theDate}.xlsx");
+            Microsoft.Office.Interop.Excel.Application oApp;
+            Microsoft.Office.Interop.Excel.Worksheet oSheet;
+            Microsoft.Office.Interop.Excel.Workbook oBook;
+
+
+            oApp = new Microsoft.Office.Interop.Excel.Application();
+            oBook = oApp.Workbooks.Add();
+            oSheet = (Microsoft.Office.Interop.Excel.Worksheet)oBook.Worksheets.get_Item(1);
+
+            int i = 0;
+            i++;
+
+            for (int j = 0; j < listBox1.Items.Count; j++)
+            {
+                oSheet.Cells[j+1, 1] = listBox1.Items[j].ToString().Split(new char[] { ' ' });
+                oSheet.Cells[j+1, 2] = listBox1.Items[j].ToString().Split(new char[] { ' ' });
+            }
+
+            foreach (var s in oSheet.Cells)
+            {
+                Console.WriteLine(s);
+            }
+            oBook.SaveAs(ExcelFileLocation);
+            oBook.Close();
+            oApp.Quit();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+
+            XmlReader file;
+            file = XmlReader.Create($"https://www.cbar.az/currencies/{theDate}.xml", new XmlReaderSettings());
+            DataSet ds = new DataSet();
+            ds.ReadXml(file);
+            dataGridView1.DataSource = ds.Tables[2];
+            dataGridView1.Columns[0].Visible = false;
+            dataGridView1.Columns.Add("Column", "Tarix");
+
+        }
+    }
+}
